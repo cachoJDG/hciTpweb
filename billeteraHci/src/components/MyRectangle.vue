858f60654e9ce3f7bc1d828dbe106e19513f5c6f
cachoJDG <jgago@itba.edu.ar>
2024-10-07 10:02:59 -0300

    Add my-rectangle

---
+++ billeteraHci/src/components/MyRectangle.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-card class="white--text pa-4" color="white" outlined>
+      <v-card-title class="black--text">{{ title }}</v-card-title>
+      <v-card-text>
+        <slot></slot>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-card {
+  border-radius: 8px;
+}
+</style>
