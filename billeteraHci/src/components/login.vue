e0c46d9395ad29b572a5a671347cce926038368f
Juan Bautista Albertoni Salini <juanbaalbertoni@gmail.com>
2024-10-07 16:57:40 -0300

    Probando cosas con la login, me queda mucho por aprender

---
+++ billeteraHci/src/components/login.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-container class="fill-height gradient-background" fluid>
+        <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+                <v-card class="login-card">
+                    <v-card-title class="headline text-black">Login</v-card-title>
+                    <v-card-text>
+                        <v-form ref="form" v-model="valid">
+                            <v-text-field 
+                                v-model="email"
+                                label="Email"
+                                required
+                                class="text-black"
+                                :rules="[v => !!v || 'Email is required']"
+                            ></v-text-field>
+                            <v-text-field
+                                v-model="password"
+                                label="Password"
+                                type="password"
+                                required
+                                class="text-black"
+                                :rules="[v => !!v || 'Password is required']"
+                            ></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" @click="login">Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+
+const login = () => {
+    // Handle login logic here
+    console.log('Email:', email.value);
+    console.log('Password:', password.value);
+};
+</script>
+
+<style scoped>
+.fill-height {
+    height: 100vh;
+}
+
+.gradient-background {
+    background: linear-gradient(to bottom, violet, grey);
+}
+
+.login-card {
+    background-color: lightgrey;
+}
+</style>
--- billeteraHci/src/plugins/vuetify.js
+++ billeteraHci/src/plugins/vuetify.js
@@ -7,6 +7,8 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import colors from 'vuetify/util/colors'
+
 
 // Composables
 import { createVuetify } from 'vuetify'
@@ -14,6 +16,15 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'dark',
+    defaultTheme: 'customTheme',
+    themes: {
+      customTheme: {
+        dark: true,
+        colors: {
+          primary: colors.purple.darken1,  // Usa purple con su tonalidad
+          secondary: colors.pink.accent2,  // Usa pink con su tonalidad
+        },
+      },
+    },
   },
 })
