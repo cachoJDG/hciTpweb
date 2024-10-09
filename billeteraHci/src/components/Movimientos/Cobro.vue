5b2b957c783d02acbd37e0e524ccd24436399393
vtn <toribioflash@gmail.com>
2024-10-09 00:01:13 -0300

    componentes para cada tipo de movimiento creados

---
+++ billeteraHci/src/components/Movimientos/Cobro.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-list-item>
+        <v-card class="bg-deep-purple-lighten-3 text-black">
+            <div>
+                <v-row justify="space-evenly">
+                    <v-col cols="2">
+                        <v-card-item class="text-h6 font-weight-bold pb-0 ma-0">Transferencia</v-card-item>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-card-item class="text-h6 text-teal-lighten-1 font-weight-bold pb-0 ma-0">+$ {{ monto
+                            }}</v-card-item>
+                    </v-col>
+                </v-row>
+            </div>
+            <div>
+                <v-row justify="space-evenly">
+                    <v-col cols="2">
+                        <v-card-item class="pt-0 ma-0">{{ persona }}</v-card-item>
+                    </v-col>
+                    <v-col cols="2">
+                        <v-card-item class="pt-0 pl-4 ma-0">{{ fecha }}</v-card-item>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-card>
+    </v-list-item>
+</template>
+
+<script>
+export default {
+    props: {
+        monto: {
+            type: String,
+        },
+        persona: {
+            type: String,
+        },
+        fecha: {
+            type: String,
+        },
+
+    }
+}
+</script>
