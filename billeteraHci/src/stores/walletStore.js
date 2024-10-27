588861a951d53a36eab734f50dd4ff7dc3d2c95a
cachoJDG <jgago@itba.edu.ar>
2024-10-27 18:06:24 -0300

    add invest functionality

--- billeteraHci/src/stores/walletStore.js
+++ billeteraHci/src/stores/walletStore.js
@@ -4,11 +4,13 @@ import { useCreditCardStore } from './creditCardStorage.js'
 
 export const useWalletStore = defineStore('wallet', () => {
     const balanceValue = ref(0)
+    const investedAmountValue = ref(0)
     const transactions = ref([])
 
     const { addCreditCard, removeCreditCard, getUserCreditCards } = useCreditCardStore()
 
     const balance = computed(() => balanceValue.value)
+    const investedAmount = computed(() => investedAmountValue.value)
 
     const addMoney = (amount, typeOfTransaction) => {
         if (amount > 0) {
@@ -30,6 +32,30 @@ export const useWalletStore = defineStore('wallet', () => {
         return false;
     }
 
+    const investMoney = (amount) => {
+        if (amount > 0 && balanceValue.value >= amount) {
+            balanceValue.value -= amount
+            investedAmountValue.value += amount
+            const date = new Date()
+            const formattedDate = `${date.toLocaleDateString()} ${date.toTimeString().split(' ')[0].slice(0, 5)}`
+            transactions.value.push({ type: 'invest', amount, date: formattedDate })
+            return true;
+        }
+        return false;
+    }
+
+    const withdrawInvestment = (amount) => {
+        if (amount > 0 && investedAmountValue.value >= amount) {
+            investedAmountValue.value -= amount
+            balanceValue.value += amount
+            const date = new Date()
+            const formattedDate = `${date.toLocaleDateString()} ${date.toTimeString().split(' ')[0].slice(0, 5)}`
+            transactions.value.push({ type: 'withdraw', amount, date: formattedDate })
+            return true;
+        }
+        return false;
+    }
+
     const payWithCreditCard = (cardNumber, amount) => {
         const userCreditCards = getUserCreditCards()
         const card = userCreditCards.find(card => card.cardNumber === cardNumber)
@@ -46,9 +72,13 @@ export const useWalletStore = defineStore('wallet', () => {
         return balance.value
     }
 
+    const getInvestedAmount = () => {
+        return investedAmount.value
+    }
+
     const getTransactions = () => {
         return transactions.value
     }
 
-    return { balance, addMoney, removeMoney, payWithCreditCard, getBalance, getTransactions }
+    return { balance, investedAmount, addMoney, removeMoney, investMoney, withdrawInvestment, payWithCreditCard, getBalance, getInvestedAmount, getTransactions }
 })
--- billeteraHci/src/views/InversionesView.vue
+++ billeteraHci/src/views/InversionesView.vue
@@ -1,49 +1,52 @@
 <script setup>
 
 import {ref} from "vue";
+import { useWalletStore } from '@/stores/walletStore.js';
+
+const walletStore = useWalletStore();
 
 const showInvestPopup = ref(false);
 
-const investedAmmount = ref(0);
-const amountToInvest = ref(0);
+const investedAmmount = ref(walletStore.getInvestedAmount());
+const amountToInvest = ref(null);
 
 const showWithdrawPopup = ref(false);
-const ammountToWithdraw = ref(0);
+const ammountToWithdraw = ref(null);
 
 const fechas = ref([
-  "27/09/2021",
-  "28/09/2021",
-  "29/09/2021",
+  { fecha: "1/08/2024", ganancia: 300 },
+  { fecha: "1/09/2024", ganancia: 700 },
+  { fecha: "1/10/2024", ganancia: 1000 },
 ]);
 
-
-
-
-
-const totalMoney = ref(5000);
+const totalMoney = ref(walletStore.getBalance());
 import MyRectangle from '@/components/GeneralUse/MyRectangle.vue';
 import MyButton from '@/components/GeneralUse/myButton.vue';
 import BarraLateral from "@/components/ContextComponents/BarraLateral.vue";
 import ContextHeader from "@/components/ContextComponents/contextHeader.vue";
 
-
-
-
 function handleInvest() {
     showInvestPopup.value = false;
-    console.log('invested: ', ammountToWithdraw.value);
-    investedAmmount.value += amountToInvest.value;
-    amountToInvest.value = 0;
-    totalMoney.value -= investedAmmount.value;
+    console.log('invested: ', amountToInvest.value);
+    if (walletStore.investMoney(parseInt(amountToInvest.value))) {
+        investedAmmount.value = walletStore.getInvestedAmount();
+        amountToInvest.value = null;
+        totalMoney.value = walletStore.getBalance();
+    } else {
+        console.error('Not enough balance to invest');
+    }
 }
 
 function handleWithdraw() {
   showWithdrawPopup.value = false;
-  console.log('withdrawed: ', investedAmmount.value);
-  investedAmmount.value -= ammountToWithdraw.value;
-  totalMoney.value += ammountToWithdraw.value;
-  ammountToWithdraw.value = 0;
-
+  console.log('withdrawed: ', ammountToWithdraw.value);
+  if (walletStore.withdrawInvestment(parseInt(ammountToWithdraw.value))) {
+      investedAmmount.value = walletStore.getInvestedAmount();
+      totalMoney.value = walletStore.getBalance();
+      ammountToWithdraw.value = null;
+  } else {
+      console.error('Not enough invested amount to withdraw');
+  }
 }
 
 </script>
@@ -52,8 +55,6 @@ function handleWithdraw() {
     <ContextHeader title="Inversiones" />
     <BarraLateral />
 
-
-
   <v-dialog v-model="showInvestPopup" max-width="400px" class="custom-dialog">
     <v-card class="pa-0 ma-0">
       <v-card-title class="headline">Invertir dinero</v-card-title>
@@ -61,7 +62,6 @@ function handleWithdraw() {
       <v-card-text class="text-h5 font-weight-bold pt-0 pb-5 ma-0" style="color: #14808c;">{{ investedAmmount }}$</v-card-text>
       <v-card-text class="text-h7 font-weight-medium pb-0">Tienes disponibles para invertir: </v-card-text>
       <v-card-text class="text-h5 font-weight-bold pt-0 pb-5 ma-0" style="color: #4A148C;">{{ totalMoney }}$</v-card-text>
-<!--      <v-card-text class="text-h7 font-weight-medium mb-0">¿Cuanto deseas inverir?</v-card-text>-->
       <v-card-text class="mt-0">
         <v-form>
           <v-text-field v-model="amountToInvest" label="¿Cuanto deseas inverir?" type="number" outlined dense class="mt-2"></v-text-field>
@@ -96,8 +96,6 @@ function handleWithdraw() {
     </v-card>
   </v-dialog>
 
-
-
     <v-row align="center" justify="space-evenly">
         <v-col cols="auto">
             <MyRectangle title="Dinero invertido total">
@@ -113,20 +111,16 @@ function handleWithdraw() {
             </MyRectangle>
             <MyRectangle title="Rendimientos recientes">
 
-
-            <v-card class="bg-deep-purple-lighten-3 mb-3 pa rendimientoCard" v-for="(fecha, index) in fechas">
+            <v-card class="bg-deep-purple-lighten-3 mb-3 pa rendimientoCard" v-for="(item, index) in fechas" :key="index">
               <v-row >
                 <v-col cols="auto">
-                  <v-card-text class="text-black text-h6">{{fecha}}</v-card-text>
+                  <v-card-text class="text-black text-h6">{{item.fecha}}</v-card-text>
                 </v-col>
                 <v-col >
-                  <v-card-text class="text-h6 text-end font-weight-bold" style="color: #14808c;">+ 300$</v-card-text>
+                  <v-card-text class="text-h6 text-end font-weight-bold" style="color: #14808c;">+ {{item.ganancia}}$</v-card-text>
                 </v-col>
-
               </v-row>
-
             </v-card>
-
 
             </MyRectangle>
         </v-col>
